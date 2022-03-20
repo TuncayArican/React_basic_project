8b1810aaa7baa877cc56dbead779394a1d604479
TuncayArican <tuncayarican07@gmail.com>
2022-03-20 17:49:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Msg from "./Msg";
+
+function App() {
+  return (
+    <div>
+      <Msg />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Msg.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+const pStyle = {
+  display: "flex",
+  height: "100vh",
+  backgroundColor: "aqua",
+  color: "purple",
+  flexDirection: "column",
+  justifyContent:"center",
+  alignItems:"center",
+};
+const newDate = `${new Date().toLocaleTimeString()}`;
+const newHour = `${new Date().toDateString()}`
+
+function Msg() {
+  return (
+
+    <div style={pStyle}>
+      <h1>{newHour}</h1>
+      <h1>{newDate}</h1>    
+    </div>
+  );
+}
+
+export default Msg;
